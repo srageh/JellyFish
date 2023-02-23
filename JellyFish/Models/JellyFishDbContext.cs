f1bb30208f605562416827743108b58226e7e8db
Khachig <khachKerbabian@gmail.com>
2023-02-22 19:09:25 -0600

    remved string coonection from db context

--- JellyFish/Models/JellyFishDbContext.cs
+++ JellyFish/Models/JellyFishDbContext.cs
@@ -45,9 +45,10 @@ public partial class JellyFishDbContext : DbContext
 
     public virtual DbSet<UserSkill> UserSkills { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-V5RNL5UO\\SQLEXPRESS;Database=JellyFishDB;User Id=sa;Password=admin123;Trusted_Connection=True;MultipleActiveResultSets=True;TrustServerCertificate=True;");
+
+
+
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
